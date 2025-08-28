95a0458139a18cf6c63d4f98ab9279e44594c536
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-08-28 12:16:03 +0300

    install packages

---
+++ src/pages/RegistrationPage.tsx
@@ -0,0 +1,11 @@
+import { ModalWindow } from "@/shared/components/ModalWindow";
+
+const RegistrationPage = () => {
+    return (
+        <div className={'flex justify-center'}>
+            <ModalWindow/>
+        </div>
+    )
+}
+
+export default RegistrationPage;
---
+++ src/shared/components/ModalWindow.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/shared/ui/button";
+
+type ModalWindowProps = object
+
+export const ModalWindow = ({}: ModalWindowProps) => {
+    return (
+        <div>
+            <Button/>
+        </div>
+    )
+}
